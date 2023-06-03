dee03e70e5a420504057e3b6825fcefb461ef803
isakya <1016361268@qq.com>
2023-06-04 01:17:22 +0800

    查看和发布审批模板功能

--- src/views/processSet/processTemplate/list.vue
+++ src/views/processSet/processTemplate/list.vue
@@ -64,6 +64,11 @@
           <el-button
             type="text"
             size="mini"
+            @click="show(scope.row)"
+          >查看审批设置</el-button>
+          <el-button
+            type="text"
+            size="mini"
             @click="edit(scope.row.id)"
             :disabled="$hasBP('bnt.processTemplate.templateSet') === false"
           >修改审批设置</el-button>
@@ -73,6 +78,13 @@
             @click="removeDataById(scope.row.id)"
             :disabled="$hasBP('bnt.processTemplate.remove') === false"
           >删除</el-button>
+          <el-button
+            v-if="scope.row.status == 0"
+            type="text"
+            size="mini"
+            @click="publish(scope.row.id)"
+            :disabled="$hasBP('bnt.processTemplate.publish') === false"
+          >发布</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -87,6 +99,50 @@
       @current-change="fetchData"
       @size-change="changeSize"
     />
+    <el-dialog
+      title="查看审批设置"
+      :visible.sync="formDialogVisible"
+      width="35%"
+    >
+      <h3>基本信息</h3>
+      <el-divider />
+      <el-form
+        ref="flashPromotionForm"
+        label-width="150px"
+        size="small"
+        style="padding-right: 40px;"
+      >
+        <el-form-item
+          label="审批类型"
+          style="margin-bottom: 0px;"
+        >{{ processTemplate.processTypeName }}</el-form-item>
+        <el-form-item
+          label="名称"
+          style="margin-bottom: 0px;"
+        >{{ processTemplate.name }}</el-form-item>
+        <el-form-item
+          label="创建时间"
+          style="margin-bottom: 0px;"
+        >{{ processTemplate.createTime }}</el-form-item>
+      </el-form>
+      <h3>表单信息</h3>
+      <el-divider />
+      <div>
+        <form-create
+          :rule="rule"
+          :option="option"
+        ></form-create>
+      </div>
+      <span
+        slot="footer"
+        class="dialog-footer"
+      >
+        <el-button
+          @click="formDialogVisible = false"
+          size="small"
+        >取 消</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 <script>
@@ -95,6 +151,10 @@ import api from '@/api/process/processTemplate'
 export default {
   data() {
     return {
+      rule: [],
+      option: {},
+      processTemplate: {},
+      formDialogVisible: false,
       listLoading: true, // 数据是否正在加载
       list: null, // banner列表
       total: 0, // 数据库中的总记录数
@@ -111,6 +171,18 @@ export default {
   mounted() {
   },
   methods: {
+    publish(id) {
+      api.publish(id).then(response => {
+        this.$message.success('发布成功')
+        this.fetchData(this.page)
+      })
+    },
+    show(row) {
+      this.rule = JSON.parse(row.formProps)
+      this.option = JSON.parse(row.formOptions)
+      this.processTemplate = row
+      this.formDialogVisible = true
+    },
     // 当页码发生改变的时候
     changeSize(size) {
       this.limit = size
